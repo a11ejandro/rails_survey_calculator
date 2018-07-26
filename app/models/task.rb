e20fa1657bba7c9889af874e18cbab6c2c4c4bf3
a11ejandro <bendthe@gmail.com>
2018-07-26 17:53:26 +0300

    Push new task id to go queue

--- app/models/task.rb
+++ app/models/task.rb
@@ -2,7 +2,11 @@ class Task < ApplicationRecord
   after_create :process_in_background
 
   private
+
   def process_in_background
     RubyCalculator.perform_async(id)
+    Sidekiq::Client.push('class' => 'GoSurveyCalculator',
+                         'args' => [id],
+                         'queue' => 'go_survey_calculator')
   end
 end
